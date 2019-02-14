0a747129730554428f2ea03adc2794d698d4a5b9
billysuherman <44047271+billysuherman@users.noreply.github.com>
2019-02-14 14:12:57 +0700

    Update Dialog.java

--- src/android/Dialog.java
+++ src/android/Dialog.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.widget.EditText;
-import cordova_plugin_twilio_video.R;
 
 
 
